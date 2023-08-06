fcac74f5e52fccef934baa74cdd818b85a0a2ab4
ahsanjefe <ahsanalamgir.dev1@gmail.com>
2023-08-06 17:06:10 +0100

    Adding WP Super Cache plugin

--- wp-config.php
+++ wp-config.php
@@ -97,6 +97,8 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'C:\xampp\htdocs\sanconett-wp\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'sanconettdb3' );
 
 /** Database username */
